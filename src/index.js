a68de3e82fce7cdf8f28dafec159e8de35601e0e
lesjohnpaul <55284022+lesjohnpaul@users.noreply.github.com>
2024-05-05 02:11:58 +0800

    corrections on js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 import { App } from 'components/App';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from './redux/store';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/goit-react-hw02-phonebook">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,3 +1,5 @@
+// store.js
+
 import {
   persistStore,
   persistReducer,
